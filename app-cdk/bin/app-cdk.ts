08ea62f64cebda8c1788eb1d52041c253af597f2
KyawZinLinKBZ <kyawzin.lin@kbzbank.com>
2025-03-03 07:17:10 +0000

    Initial Commit

---
+++ app-cdk/bin/app-cdk.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { AppCdkStack } from '../lib/app-cdk-stack';
+import { PipelineCdkStack } from '../lib/pipeline-cdk-stack'
+
+const app = new cdk.App();
+
+const testCdkStack = new AppCdkStack(app, 'test', {});
+
+const pipelineCdkStack = new PipelineCdkStack(app, 'pipeline-stack', {});
